2bb2c70be33362f21c21e8f18adb6d0f519bcc57
SeungHo0422 <songseungho9258@gmail.com>
2022-07-18 04:38:36 +0900

    3장 - JPA를 이용한 등록/수정/조회 API 생성 및 Auditing 기능 활성화 완료

--- src/main/java/com/jojoldu/book/springboot/domain/posts/Posts.java
+++ src/main/java/com/jojoldu/book/springboot/domain/posts/Posts.java
@@ -9,7 +9,7 @@ import javax.persistence.*;
 @Getter
 @NoArgsConstructor
 @Entity //테이블과 링크될 클래스임을 annotation
-public class Posts {
+public class Posts extends BaseTimeEntity {
 
     @Id //Primary Key임을 명시
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -28,5 +28,10 @@ public class Posts {
         this.title = title;
         this.content = content;
         this.author = author;
+    }
+
+    public void update(String title, String content) {
+        this.title = title;
+        this.content = content;
     }
 }
